3d091c01ca75d97325214b75ebf70e5bb0c9ff90
paulo.soares <paulo.soares@ifsc.edu.br>
2022-05-12 22:48:53 -0300

    aula-02

--- src/ConsumidorDeString.java
+++
@@ -1,9 +0,0 @@
-import java.util.function.Consumer;
-
-public class ConsumidorDeString implements Consumer<String> {
-	
-    public void accept(String s) {
-        System.out.println(s);
-    }
-
-}
--- src/OrdenaStrings.java
+++ src/OrdenaStrings.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
@@ -13,19 +12,56 @@ public class OrdenaStrings {
 		palavras.add("casa do código");
 		palavras.add("caelum");
 		
-		Collections.sort(palavras);
-		System.out.println(palavras);
+		//classe anônima implementando um default method consumer
+		palavras.forEach(new Consumer<String>() {
+		    public void accept(String s) {
+		        System.out.println(s);
+		    }
+		});
 		
-		Comparator<String> comparador = new ComparadorDeStringPorTamanho();
-		Collections.sort(palavras, comparador);
+		//lambdas enxugando a sintaxe de classes anônimas
+		palavras.forEach((String s) -> {
+		    System.out.println(s);
+		});
 		
-		//default method sort de List
-		palavras.sort(comparador);
+		palavras.forEach((s) -> {
+		    System.out.println(s);
+		});
 		
-		Consumer<String> consumidor = new ConsumidorDeString();
-		//default method consumer de Iterable
-		palavras.forEach(consumidor);
-
+		palavras.forEach(s -> {
+		    System.out.println(s);
+		});
+		
+		palavras.forEach(s -> System.out.println(s));
+		
+		
+		//classe anônima com Comparator
+		palavras.sort(new Comparator<String>() {
+		    public int compare(String s1, String s2) {
+		        if (s1.length() < s2.length())
+		            return -1;
+		        if (s1.length() > s2.length())
+		            return 1;
+		        return 0;
+		    }
+		});
+		
+		//lambda com Comparator
+		palavras.sort((s1, s2) -> {
+		    if (s1.length() < s2.length())
+		        return -1;
+		    if (s1.length() > s2.length())
+		        return 1;
+		    return 0;
+		});
+		
+		palavras.sort((s1, s2) -> {
+		    return Integer.compare(s1.length(), s2.length());
+		});
+		
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		
+		
 	}
 
 }
