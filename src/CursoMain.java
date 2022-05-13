9c597bbd6c5bc32ba6192e1bb1fbd1405159724e
paulo.soares <paulo.soares@ifsc.edu.br>
2022-05-12 23:31:04 -0300

    aula-05

--- src/CursoMain.java
+++ src/CursoMain.java
@@ -1,56 +1,55 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CursoMain {
 
-	@SuppressWarnings("unused")
+	@SuppressWarnings({ "unused", "rawtypes" })
 	public static void main(String[] args) {
 		List<Curso> cursos = new ArrayList<Curso>();
 		cursos.add(new Curso("Python", 45));
 		cursos.add(new Curso("JavaScript", 150));
 		cursos.add(new Curso("Java 8", 113));
 		cursos.add(new Curso("C", 55));
-		
-		//comparação por quantidade de alunos
-		cursos.sort(Comparator.comparingInt(c -> c.getAlunos()));
-		cursos.sort(Comparator.comparingInt(Curso::getAlunos));
-		
-		//Streams
-		//Stream<Curso> streamDeCurso = cursos.stream();
-		
-		//Stream de cursos para encontrar cursos com mais de 100 alunos 
-		// usando um predicado de comparação com Lambda
-		Stream<Curso> streamDeCurso = cursos.stream().filter(c -> c.getAlunos() > 100);
-		streamDeCurso.forEach(c -> System.out.println(c.getNome()));
 
-		//poderia ser mais simples
+		//retorna qualquer curso com mais de 100 alunos para um Optional, pois talvez o retorno seja nulo
+		Optional<Curso> optional = cursos.stream()
+		   .filter(c -> c.getAlunos() > 100)
+		   .findAny();
+		
+		//retorna o objeto encontrado
+		Curso curso1 = optional.get();
+		
+		//retorna o objeto se existir ou outro retorno
+		Curso curso2 = optional.orElse(null);
+		
+		//chama o lambda se o objeto curso estiver presente
+		optional.ifPresent(c -> System.out.println(c.getNome()));
+		
 		cursos.stream()
 		   .filter(c -> c.getAlunos() > 100)
-		   .forEach(c -> System.out.println(c.getNome()));
-		
-		// Map para capturar um novo Stream de cursos com quantidade acima de 100 alunos
-		cursos.stream()
-		   .filter(c -> c.getAlunos() > 100)
-		   .map(Curso::getAlunos)
-		   .forEach(System.out::println);
+		   .findAny()
+		   .ifPresent(c -> System.out.println(c.getNome()));
 		
 		
-		/*
-		 * Quando fizemos o map(Curso::getAlunos), recebemos de volta um Stream<Integer>,
-		 *  que acaba fazendo o autoboxing dos ints. Isto é, utilizará mais recursos da JVM.
-		 *  Claro que, se sua coleção é pequena, o impacto será irrisório. 
-		 *  Mas é possível trabalhar só com ints, invocando o método mapToInt*/
-		IntStream stream = cursos.stream()
+		//Gerando uma coleção a partir de um Stream
+		
+		List<Curso> resultados = cursos.stream()
 				   .filter(c -> c.getAlunos() > 100)
-				   .mapToInt(Curso::getAlunos);
+				   .collect(Collectors.toList());
 		
-		int soma = cursos.stream()
-				   .filter(c -> c.getAlunos() > 100)
-				   .mapToInt(Curso::getAlunos)
-				   .sum();
+		/**
+		 * Queremos um mapa que, dado o nome do curso, o valor atrelado é a quantidade alunos.
+		 * Um Map<String, Integer>. Utilizamos o Collectors.toMap. Ele recebe duas Functions. 
+		 * A primeira indica o que vai ser a chave, e a segunda o que será o valor
+		 */
+		Map mapa = cursos 
+				.stream() 
+				.filter(c -> c.getAlunos() > 100) 
+				.collect(Collectors.toMap(c -> c.getNome(), c -> c.getAlunos()));
+		
 	}
 
 }
